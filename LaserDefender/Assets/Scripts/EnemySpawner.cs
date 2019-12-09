bd2a974fe9409735114dd685930701fb1786626e
ilpeppino <ilpeppino@gmail.com>
2019-12-09 16:41:58 +0100

    spawn multiple waves

--- LaserDefender/Assets/Scripts/EnemySpawner.cs
+++ LaserDefender/Assets/Scripts/EnemySpawner.cs
@@ -15,16 +15,25 @@ public class EnemySpawner : MonoBehaviour
 {
 
     [SerializeField] List<WaveConfig> waveConfigs;
-    int startingWave = 0;
+    [SerializeField] int startingWave = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         // Calls a coroutine that spawns enemies within an indexed wave
-        var currentWave = waveConfigs[startingWave];
-        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+        
+        StartCoroutine(SpawnAllWaves());
     }
 
+    private IEnumerator SpawnAllWaves()
+    {
+        for (int waveIndex = startingWave; waveIndex < waveConfigs.Count; waveIndex++)
+        {
+            var currentWave = waveConfigs[waveIndex];
+            yield return StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+
+        }
+    }
     private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
     {
         // Iterates per nr of enemies times defined in Wave x
