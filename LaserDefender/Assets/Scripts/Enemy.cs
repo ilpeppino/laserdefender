cc2a40be4e5c382716fe575c2a4d0f2b24a0c08e
ilpeppino <ilpeppino@gmail.com>
2019-12-11 15:43:08 +0100

    generate continuous waves of two types of shooting enemies

---
+++ LaserDefender/Assets/Scripts/Enemy.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    [SerializeField] float health = 100;
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] float laserSpeed = 1f;
+
+    // We can map the Laser prefab into the Player object
+    [SerializeField] GameObject enemyLaserPrefab;
+    
+    private void Start()
+    {
+        shotCounter = UnityEngine.Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+    }
+
+    private void Update()
+    {
+        CountDownAndShoot();
+    }
+
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+
+        if (shotCounter <= 0)
+        {
+            Fire();
+            shotCounter = UnityEngine.Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject enemyLaser = Instantiate(enemyLaserPrefab, transform.position, Quaternion.identity) as GameObject;
+        enemyLaser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -laserSpeed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // damageDealer stores the DamageDealer component of the object which hits the enemy
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        ProcessHit(damageDealer);
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+        health -= damageDealer.GetDamage();
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- LaserDefender/Assets/Scripts/EnemySpawner.cs
+++ LaserDefender/Assets/Scripts/EnemySpawner.cs
@@ -16,13 +16,18 @@ public class EnemySpawner : MonoBehaviour
 
     [SerializeField] List<WaveConfig> waveConfigs;
     [SerializeField] int startingWave = 0;
+    [SerializeField] bool looping = false;
 
     // Start is called before the first frame update
-    void Start()
+    IEnumerator Start()
     {
-        // Calls a coroutine that spawns enemies within an indexed wave
+        do
+        {
+            // Calls a coroutine that spawns enemies within an indexed wave
+            yield return StartCoroutine(SpawnAllWaves());
+        } while (looping);
         
-        StartCoroutine(SpawnAllWaves());
+        
     }
 
     private IEnumerator SpawnAllWaves()
